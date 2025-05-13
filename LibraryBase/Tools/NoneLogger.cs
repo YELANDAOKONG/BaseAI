6768f921981ef1d9d28ff425b52b97cdc6330958
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-05-14 03:19:14 +0800

    Update ILogger

---
+++ LibraryBase/Tools/NoneLogger.cs
@@ -0,0 +1,51 @@
+﻿using LibraryBase.Tools.Interfaces;
+
+namespace LibraryBase.Tools;
+
+public class NoneLogger : ILogger
+{
+    public void Log(string level, params string[] messages)
+    {
+        return;
+    }
+
+    public void Off(params string[] messages)
+    {
+        return;
+    }
+
+    public void Trace(params string[] messages)
+    {
+        return;
+    }
+
+    public void Debug(params string[] messages)
+    {
+        return;
+    }
+
+    public void Info(params string[] messages)
+    {
+        return;
+    }
+
+    public void Warn(params string[] messages)
+    {
+        return;
+    }
+
+    public void Error(params string[] messages)
+    {
+        return;
+    }
+
+    public void Fatal(params string[] messages)
+    {
+        return;
+    }
+
+    public void All(params string[] messages)
+    {
+        return;
+    }
+}
